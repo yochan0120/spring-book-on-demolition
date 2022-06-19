edee7081d555c04137474b9754031b873a43c5d6
yo-chan <whitesnow.wave@gmail.com>
2022-06-19 14:53:36 +0900

    all

---
+++ src/main/java/com/example/springbookondemolition/SpringBookOnDemolitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbookondemolition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookOnDemolitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookOnDemolitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbookondemolition/SpringBookOnDemolitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbookondemolition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookOnDemolitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
